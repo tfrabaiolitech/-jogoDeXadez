841bd8f4030cab35ec0e79df0acae4f4cf45cb55
Thiago Rabaioli <tfrabaioli.tech@gmail.com>
2018-10-25 22:19:04 +0100

    Projeto criado, Diretorio Tabuleiro criado e Classe posição também

---
+++ jogoDeXadez_CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace jogoDeXadez_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
+        }
+    }
+}
